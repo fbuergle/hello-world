cce37a30f6245384d676b9c6e98ecbe6b5687358
fbue <43402185+fbuergle@users.noreply.github.com>
2018-10-06 15:26:25 +0200

    TestUI2 and VuforiaTargetDetectionTest
    TestUI2: First working AR example. Button to add additional cubes, one cube with a material attached moving to a sphere which also has material attached and a collision event.
    
    VuforiaTargetDetectoinTest: Basic ImageTarget of Vuforia with the Astronaut example provided by Vuforia itself.

---
+++ TestUI2/Assets/buttonClick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonClick : MonoBehaviour {
+	public Button m_YourFirstButton;
+	
+	public GameObject myCube;
+	
+	//x and y componentes of the newly added cube
+	private float pos_x = 456.0f;
+	private	float pos_y = 2.5f;
+	private	float pos_z = -9.0f;
+	
+		
+	
+	// The target marker. --> myCube will move towards this
+    public Transform target;
+	
+	// Use this for initialization
+	void Start () {
+		
+		//Find the button
+		Button btn1 = m_YourFirstButton.GetComponent<Button>();
+
+        //Calls the TaskOnClick/TaskWithParameters method when you click the Button
+        btn1.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//from https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+        // The step size is equal to speed times frame time.
+        float step = Time.deltaTime;
+
+        // Move our position a step closer to the target.
+        myCube.transform.position = Vector3.MoveTowards(myCube.transform.position, target.position, step);
+    
+	}
+
+	void TaskOnClick(){
+		Debug.Log("Button was clicked");
+			
+		//Create new cube
+		GameObject Kube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		Kube.transform.Translate (pos_x,pos_y,pos_z);
+		pos_x -= 1.0f;
+		pos_y -= 1.0f;
+	}
+}
---
+++ TestUI2/Assets/collisionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter (Collision col){
+		Debug.Log("Collision happened " + col.gameObject.name);
+		
+		
+        //Set the main Color of the Material to green
+        GetComponent<Renderer>().material.shader = Shader.Find("_Color");
+        GetComponent<Renderer>().material.SetColor("_Color", col.gameObject.GetComponent<Renderer>().material.color);
+		//Find the Specular shader and change its Color 
+        GetComponent<Renderer>().material.shader = Shader.Find("Specular");
+        GetComponent<Renderer>().material.SetColor("_SpecColor", col.gameObject.GetComponent<Renderer>().material.color);
+		
+		//Destroy object that collided with this one
+		Destroy(col.gameObject);
+	}
+}
